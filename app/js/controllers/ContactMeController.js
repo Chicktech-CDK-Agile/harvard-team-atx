3ac2527ad56d09c2e827334608e25a421ee7f617
chicktech <cdkagile@chicktech.org>
2017-04-22 13:26:53 -0500

    Added Last Name, Email, and Phone Number to the Contact Me tab

--- app/js/controllers/ContactMeController.js
+++ app/js/controllers/ContactMeController.js
@@ -6,7 +6,10 @@ angular.module('myApp.controllers')
         navService.setActiveTab("contactme");
 
         $scope.contactinfo = {
-            FirstName: ''
+            FirstName: '',
+            LastName: '',
+            Email: '',
+            PhoneNumber: ''
         };
         $scope.contactMe = function(widget){
             var modalInstance = $modal.open({
